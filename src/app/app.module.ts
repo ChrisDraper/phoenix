01f54fe730d294c15b14aad16fc36f00d52e7776
Chris Draper <chris@moonshinenet.com>
2016-11-11 16:17:13 +0000

    Data provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
+import { Data } from '../providers/data';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -19,6 +21,6 @@ import { Page2 } from '../pages/page2/page2';
     Page1,
     Page2
   ],
-  providers: []
+  providers: [Data]
 })
 export class AppModule {}
--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {Data} from '../../providers/data';
+
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
 
+	offers: any;
   constructor(public navCtrl: NavController) {
-    
+    this.offers = {title: 'January Sale', text: 'Is on now'};
   }
 
 }
