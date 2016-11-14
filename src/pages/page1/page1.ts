68e1ca2f068f177ad973dedee8cca73d7ffbab52
Chris Draper <chris@moonshinenet.com>
2016-11-14 15:28:47 +0000

    Data using API

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
 import {Data} from '../../providers/data';
 
 @Component({
@@ -11,8 +10,40 @@ import {Data} from '../../providers/data';
 export class Page1 {
 
 	offers: any;
-  constructor(public navCtrl: NavController) {
+  account: any;
+
+
+  constructor(public navCtrl: NavController, public dataService: Data, private http: Http, public alertCtrl: AlertController) {
+    this.account = '6';
     this.offers = [{title: 'January Sale', text: 'Is on now'}, {title: 'Easter Sale', text: 'Is on a bit later'}];
+    this.getOffers();
+ }
+
+  getOffers() {
+     this.http.get('http://portal.mi-app.co.uk/connection.php?account=6')
+              .map(res => res.json())
+              .subscribe(
+                  data => {
+                    console.log('Data: ' + data[0].title);
+                    this.offers = [{title: data[0].title, text: data[0].description}];
+                    //setTimeout(() => {
+                     // this.offers = this.dataService.formatObservation(data.SiteRep.DV);
+                      
+                    //}, 1500);
+                  },
+                  error => {
+                      console.log(error)
+                      this.showConectionErrAlert();
+                  });
+  }  
+  
+  showConectionErrAlert() {
+      let alert = this.alertCtrl.create({
+        title: 'Oh bother!!',
+        subTitle: 'Connection to latest data is unavailable.',
+        buttons: ['FAIR ENOUGH']
+      });
+      alert.present();
   }
 
 }
